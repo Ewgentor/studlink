f8a7c17ace4e0fb64b9dddb8d1e4810a7eb89026
Your0ff <mr.ilya.yurov.2001@list.ru>
2025-07-07 00:47:38 +0300

    homePage

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -5,17 +5,19 @@ export const Header = () => {
   const { data: session } = useSession();
 
   return (
-    <div className="flex justify-between px-2 py-4 border-b">
-      <div>Я тебя найду и уничтожу, я просрал на поиск этого говна 3 часа</div>
-
-      <div>
+    <div className="flex justify-between px-8 py-4 border-b bg-black"> 
+      <div className="place-items-center">
+        <div className="font-bold text-white">StudLink</div>
+        <div className="bg-teal-900 text-white px-2">FREELANCER</div>
+      </div>
+      <div className="flex place-items-center">
         { session?.user ? (
-          <div className="flex flex-row gap-2">
-            <p>{ session.user.name }</p>
-            <button onClick={() => void signOut()}>Sign Out</button>
+          <div className="flex flex-row gap-10">
+            <a href="#" className="cursor-pointer text-white" >{ session.user.name }</a>
+            <button className="cursor-pointer text-white" onClick={() => void signOut()}>Sign Out</button>
           </div>
         ) : (
-          <button onClick={() => void signIn()}>Sign In</button>
+          <button className="cursor-pointer text-white" onClick={() => void signIn()}>Sign In</button>
         )}
       </div>
     </div>
